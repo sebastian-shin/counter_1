28ef52059fbd4acda7c36ea55f6c3f8c33dd7e92
sebastian-shin <1sshin@pm.me>
2021-11-27 21:44:23 +0900

    hello

---
+++ counting_1.py
@@ -0,0 +1,9 @@
+# counting something using for loops
+# count the numbers in the for loop.
+
+counter = 0
+
+for a_number in [38, 291, 30, 20, 1, 382, 382, 4]:
+    counter = counter + 1
+
+print(counter)
