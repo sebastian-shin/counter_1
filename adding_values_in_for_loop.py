4efdc28bf013dfe47bee474933641a7612c98c2c
sebastian-shin <1sshin@pm.me>
2021-11-29 12:21:56 +0900

    November 29, 2021

---
+++ adding_values_in_for_loop.py
@@ -0,0 +1,10 @@
+# this python program is to test adding values using for loop
+
+
+number_1 = 0
+
+for a_number in [1, 2, 3, 4, 5, 10, 11, 23]:
+    number_1 = number_1 + a_number
+    print(number_1)
+
+print("Final Sum ", number_1)
