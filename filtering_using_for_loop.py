feee5da41f619b305a53ebc635397bfbf1865721
sebastian-shin <1sshin@pm.me>
2021-11-29 12:41:56 +0900

    no message

---
+++ filtering_using_for_loop.py
@@ -0,0 +1,7 @@
+# finding certain values in the list using for loop
+
+number_1 = 0
+
+for a_number in [1, 100, 101, 50, 102, 104, 60 ,38, 20]:
+    if a_number > 100:
+        print("This was higher than one hundred -> ", a_number)
