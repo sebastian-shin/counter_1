9f0e4bb20666989332ca26802d27f0c1fe009946
honggwan <1sshin@pm.me>
2021-12-01 09:15:12 +0900

    no comment no comment

--- searching_using_boolean_variable.py
+++ searching_using_boolean_variable.py
@@ -1,12 +1,4 @@
 #Below code finds whether or not we got something matching to what we're looking for
 
-true_or_false = False
-print('Before', true_or_false)
-for a_number in [9, 8, 39, 39, 382, 100, 2, 200, 200, 300]:
-    if a_number == 200:
-        true_or_false = True
-    print(a_number, true_or_false)
-print('After', true_or_false)
-#
-
-
+# testing upload
+# testing upload
